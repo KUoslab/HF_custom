ee792e0ac15536d5b8f8c53d9fc6c5a7e69ccf30
kwanhoonlee <rok.lee91@gmail.com>
2020-04-07 12:22:14 +0900

    clean

--- app/routes/hash.js
+++ app/routes/hash.js
@@ -1,11 +1,13 @@
 var crypto = require('crypto');
 
-const getHash = function(text){
-    var shasum = crypto.createHash('sha1');
-    shasum.update(text);
-    var output = shasum.digest('hex');
+module.exports = {
+    getHash: function(text){
+        var shasum = crypto.createHash('sha1');
+        shasum.update(text);
+        var output = shasum.digest('hex');
     
-    return output
+        return output
+    }
 }
 
-module.exports = getHash
+
